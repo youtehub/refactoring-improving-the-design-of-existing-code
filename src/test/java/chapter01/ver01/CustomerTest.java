13bd5e1579a41d29da4355a5d7decfa18c151768
javaprocj <2728142607@qq.com>
2021-07-09 00:56:47 +0800

    常客积分计算

--- src/test/java/chapter01/ver01/CustomerTest.java
+++ src/test/java/chapter01/ver01/CustomerTest.java
@@ -4,16 +4,19 @@ import org.testng.annotations.Test;
 
 public class CustomerTest {
 
-	@Test
-	public void statement() {
-		Customer customer = new Customer("John");
-		String title = "Titanic";
-		int priceCode = 2;
-		int _daysRented = 7;
-		Movie movie = new Movie(title, priceCode);
-		Rental rental = new Rental(movie, _daysRented);
-		customer.addRental(rental);
-		String result = customer.statement();
-		System.out.println(result);
-	}
+    @Test
+    public void statement() {
+        Customer customer = new Customer("John");
+        String title = null;
+        int priceCode = 2;
+        int _daysRented = 7;
+        Movie movie = new Movie(title, priceCode);
+        Rental rental = new Rental(movie, _daysRented);
+        customer.addRental(rental);
+        String result = customer.statement();
+        System.out.println(result);
+
+    }
+
+
 }
--- src/test/java/pracs/chapter01/ver03/CustomerTest.java
+++ src/test/java/pracs/chapter01/ver03/CustomerTest.java
@@ -16,6 +16,17 @@ public class CustomerTest {
     }
 
     @Test
+    public void statement01_1() {
+        Movie movie = new Movie("A Dog's Way Home", Movie.CHILDRENS);
+        Rental rental = new Rental(movie, 7);
+
+        CustomerRefac customer = new CustomerRefac("Tom");
+        customer.addRental(rental);
+        String result = customer.statement();
+        System.out.println(result);
+    }
+
+    @Test
     public void statement02() {
         Movie movie = new Movie("Boss Level", Movie.REGULAR);
         Rental rental = new Rental(movie, 7);
@@ -27,11 +38,33 @@ public class CustomerTest {
     }
 
     @Test
+    public void statement02_1() {
+        Movie movie = new Movie("Boss Level", Movie.REGULAR);
+        Rental rental = new Rental(movie, 7);
+
+        CustomerRefac customer = new CustomerRefac("William");
+        customer.addRental(rental);
+        String result = customer.statement();
+        System.out.println(result);
+    }
+
+    @Test
     public void statement03() {
         Movie movie = new Movie("Titanic", Movie.NEW_RELEASE);
         Rental rental = new Rental(movie, 7);
 
         Customer customer = new Customer("Audrey");
+        customer.addRental(rental);
+        String result = customer.statement();
+        System.out.println(result);
+    }
+
+    @Test
+    public void statement03_1() {
+        Movie movie = new Movie("Titanic", Movie.NEW_RELEASE);
+        Rental rental = new Rental(movie, 7);
+
+        CustomerRefac customer = new CustomerRefac("Audrey");
         customer.addRental(rental);
         String result = customer.statement();
         System.out.println(result);
